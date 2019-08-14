a82eb567c2dec2c979503604d1713c4b2fb0cce7
kwdowns <12downsk@gmail.com>
2019-08-14 16:19:59 -0500

    update generate script to remove container after build

---
+++ Generate-SDK.psm1
@@ -0,0 +1,82 @@
+﻿<#
+.SYNOPIS
+This script will generate an SDK client for the given api
+
+.EXAMPLE
+./Generate-SDK.ps1 `
+  -SwaggerUrl 'http://localhost:80/swagger/v1/swagger.json' `
+  -SDKClientPath '
+#>
+function Generate-SDK {
+param(
+    [String] $SwaggerUrl,
+    [System.IO.FileInfo] $SDKClientPath,
+    [String] $DomainOwner,
+    [String] $ServiceName,
+    [String[]] $Scopes,
+    [String] $ModelPackage = "Models",
+    [String] $ApiPackage = "Api",
+    [Int] $PackageMajorVersion = 1,
+    [Int] $PackageMinorVersion = 0,
+    [String] $IdentityModelVersion = "3.10.10",
+    [String] $NitoAsyncExCoordinationVersion = "5.0.0",
+    [String] $RefitVersion = "4.7.9",
+    [String] $RefitHttpClientFactoryVersion = "4.7.9",
+    [String] $RetailSuccessAuthenticationTokensVersion = "1.0.1",
+    [String] $RetailSuccessSDKCoreVersion = "1.0.0"
+)
+
+docker pull retailsuccess/swagger-codegen
+
+#Config values
+$Options=@{ }
+$Options.Add('domainOwner',$DomainOwner)
+$Options.Add('serviceName', $ServiceName)
+$Options.Add('modelPackage', $ModelPackage)
+$Options.Add('apiPackage', $ApiPackage)
+$Options.Add('packageMajorVersion', $PackageMajorVersion)
+$Options.Add('packageMinorVersion', $PackageMinorVersion)
+$Options.Add('apiScopesCommaSeperated', ($Scopes -join ","))
+$Options.Add('packageVersionIdentityModel', $IdentityModelVersion)
+$Options.Add('packageVersionNitoAsyncExCoordination', $NitoAsyncExCoordinationVersion)
+$Options.Add('packageVersionRefit', $RefitVersion)
+$Options.Add('packageVersionRefitHttpClientFactory', $RefitHttpClientFactoryVersion)
+$Options.Add('packageVersionRetailSuccessAuthenticationTokens', $RetailSuccessAuthenticationTokensVersion)
+$Options.Add('packageVersionRetailSuccessSDKCore', $RetailSuccessSDKCoreVersion)
+
+mkdir $tempFolder -Force
+Get-ChildItem $tempFolder -Recurse | Remove-Item
+
+#create a temp folder
+$tempFolder=Join-Path -Path $pwd.Path -ChildPath ".tmp"
+
+#location for configuration files to mount inside the container
+$configFolder=Join-Path -Path $tempFolder -ChildPath "config"
+$configPath=Join-Path -Path $configFolder -ChildPath "config.json"
+
+#location for codegen to place output files
+$outFolder=Join-Path -Path $tempFolder -ChildPath "out"
+
+mkdir $configFolder -Force
+mkdir $outFolder -Force
+
+$Options | ConvertTo-Json | Out-File $configPath
+
+docker run `
+    --rm `
+    -v ${configFolder}:/opt/swagger-codegen/config `
+    -v ${outFolder}:/opt/swagger-codegen/out `
+    retailsuccess/swagger-codegen generate `
+        -i $SwaggerUrl `
+        -l rsCsharp `
+        -c /opt/swagger-codegen/config/config.json `
+        -o /opt/swagger-codegen/out `
+        --additional-properties excludeTests=true
+
+#clear out current contents of client folder, then copy new client files over
+Get-ChildItem $SDKClientPath -Recurse | Remove-Item
+Copy-Item $outFolder -Destination $SDKClientPath -Recurse -Container
+
+#clean up temp folder
+Remove-Item $tempFolder -Recurse -Force
+}
